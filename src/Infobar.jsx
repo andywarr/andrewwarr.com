3769f16024a83e4d75b63249172c61dc83104ea1
Andy Warr <andywarr@gmail.com>
2023-03-11 10:41:40 -0800

    Updated infobar message

--- src/Infobar.jsx
+++ src/Infobar.jsx
@@ -12,7 +12,7 @@ export default class Infobar extends React.Component {
         return (
             <div className="infobar">
                 <div>
-                    <div>These tiles were made to move</div>
+                    <div>This site will use your device orientation if you'll let it</div>
                     <div>
                         <button id="requestDeviceOrientation" onClick = {this.props.handleDeviceOrientationPermission} role="button">Allow</button>
                     </div>
